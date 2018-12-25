838bbf8519039a2bb432e3fcdab32278ccebcba6
lianchangzhang <15826873797@163.com>
2018-12-25 14:17:11 +0800

    正确引入插件问题

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ export default {
      plugins: [
           [
                // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
-               'umi-plugin-react': {}
+               'umi-plugin-react', {}
           ],
      ],
      routes: [
