11cb1113679f9ba5f1bbc3b9bfc6de4c3c680696
lianchangzhang <15826873797@163.com>
2018-12-25 14:09:18 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+export default {
+     plugins: [
+          [
+               // 这里暂时还没有添加配置，该插件还不会有作用，我们会在后面的课程按照需求打开相应的配置
+               'umi-plugin-react': {}
+          ],
+     ],
+     routes: [
+          {
+               path: '/',
+               component: './Helloworld'
+          }
+     ]
+};
